e39e1851c08cae7b33bbc7f66106b56452f10d8f
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-10-27 11:41:25 +0100

    initial commit

---
+++ digital_doorbell/rpi_doorbell/app.js
@@ -0,0 +1,11 @@
+// Start
+var express = require('express');
+var app = express();
+var piREST = require('pi-aREST')(app);
+
+piREST.set_id('34f5eQ');
+piREST.set_name('rpi');
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
