065ab3b734006e078e80c46f515873fb04afa867
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-10-28 15:43:33 +0100

    fixed index.js

--- digital_doorbell/rpi_doorbell/app.js
+++ digital_doorbell/rpi_doorbell/app.js
@@ -1,18 +1,19 @@
 // Start
 var express = require('express');
 var app = express();
-var piREST = require('pi-arest')(app);
-var Sound = require('node-aplay');
-
-// Set parameters
-piREST.set_id('34f5eQ');
-piREST.set_name('rpi');
 
 // Route for ringbell
 app.get('/doorbell', function(req, res) {
   new Sound('doorbell.wav').play();
   res.json({data: "Sound played"});
 });
+
+var piREST = require('pi-arest')(app);
+var Sound = require('node-aplay');
+
+// Set parameters
+piREST.set_id('34f5eQ');
+piREST.set_name('rpi');
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
